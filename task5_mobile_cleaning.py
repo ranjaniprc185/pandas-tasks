db6d82639980b582137374ebfce038806f8254d5
ranjaniprc185 <161228228+ranjaniprc185@users.noreply.github.com>
2025-12-10 22:27:50 +0530

    Create task5_mobile_cleaning.py

---
+++ task5_mobile_cleaning.py
@@ -0,0 +1,14 @@
+import pandas as pd
+data=pd.read_csv("C:\\Users\\ranja\\New folder (4)\\Smartphones.csv")
+df=pd.DataFrame(data)
+df.replace(['n/a','NA'],pd.NA,inplace=True)
+df.fillna("unknown",inplace=True)
+print(df)
+mobiles=df[['brand_name','model','price','avg_rating','ram_capacity']].copy()
+print(mobiles)
+mobiles=mobiles.iloc[:-200,:]
+print(mobiles)
+print(len(mobiles))
+mobiles=mobiles[mobiles['ram_capacity']!="unknown"]
+print(mobiles)
+print(len(mobiles))
